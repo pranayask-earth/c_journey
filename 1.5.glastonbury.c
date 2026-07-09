4e2b8235ae343935a53c686d46a103cac9c3fd1b
PranayaSK-earth <pranayask.earth@gmail.com>
2026-07-09 18:51:26 +0000

    1.5.glastonbury.c

---
+++ 1.5.glastonbury.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+int c;
+printf("type a character (or press ctrl+d for EOF):\n");
+if (c = (getchar() != EOF) ==1)
+{
+printf("the value is: %d\n",c);
+}
+else
+{
+printf("the value doesn't exist ;)\n");
+}
+}
+
+
+
